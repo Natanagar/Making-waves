6e6d4b26ad89c3659648d4e7c8764057e4fff078
Ciemna_noc <agarkowa@gmail.com>
2019-06-29 01:56:55 +0200

    add audio

--- src/App.js
+++ src/App.js
@@ -5,16 +5,25 @@ function App() {
   return (
     <>
       <header>Audio player</header>
+      <form>Autentification</form>
       <section>
         <form>
           <label htmlFor="track" />
           <input type="text" id="track" required defaultValue="Choose your favourite music" />
         </form>
         <form>
-          <label />
-          <audio>
-            <input />
-          </audio>
+          <figure>
+            <figcaption>Listen your favourite music</figcaption>
+            <audio
+              controls
+              src="/media/examples/t-rex-roar.mp3"
+            >
+              Your browser does not support the
+              <code>audio</code>
+              element.
+            </audio>
+          </figure>
+
         </form>
       </section>
     </>
