3aebd5891395dc5d1858eb0d8b17bd942f3ac5a0
Ciemna_noc <agarkowa@gmail.com>
2019-06-29 14:48:10 +0200

    authentification

---
+++ src/components/Authentification/Form.jsx
@@ -0,0 +1,19 @@
+import React, {
+  PureComponent,
+} from 'react';
+
+export const AuthForm = () => (
+  <section>
+    <form>
+      <label htmlFor="" />
+      <input
+type="email"
+        required
+        defaultValue="google@gmail.com"
+      />
+
+
+    </form>
+
+  </section>
+);
